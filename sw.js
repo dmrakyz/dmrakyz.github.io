f77a559a537966ef93053185d50da8a596940aa7
dmrakyz <92512561+dmrakyz@users.noreply.github.com>
2024-12-02 22:42:51 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,46 @@
+const CACHE_NAME = "pwa-cache-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/app.js",
+  "/main.js",
+  "/icon-192.png",
+  "/icon-512.png",
+  "/screenshot1.png",
+  "/screenshot2.png",
+];
+
+// Install Service Worker
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Activate Service Worker
+self.addEventListener("activate", (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Fetch requests
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
